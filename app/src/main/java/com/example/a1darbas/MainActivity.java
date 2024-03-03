38a66721f28766ae3a36ffc1aea596786647c7dc
GedasLTU <luksagediminas19@gmail.com>
2024-03-03 15:47:41 +0200

    Mygtukas keicia teksta, kitas - spalva

--- app/src/main/java/com/example/a1darbas/MainActivity.java
+++ app/src/main/java/com/example/a1darbas/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.a1darbas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textView = findViewById(R. id. textView);
+    }
+
+    public void spustelejus(View view) {
+        this.textView.setText("labas");
+    }
+
+    public void nuspalvoti(View view) {
+        this.textView.setTextColor(Color.parseColor("#FF0000"));
     }
 }
